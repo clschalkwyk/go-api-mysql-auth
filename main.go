b37668985e4de56496f0b6b19a5390beeb3217a6
wikusSchalkwyk <wikus@hyvemobile.co.za>
2022-06-18 22:00:15 +0200

    Added gin; test endpoint

--- main.go
+++ main.go
@@ -3,14 +3,30 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+func ConnectDb() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Start")
-	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3309)/authdb")
+	db, err := sql.Open("mysql",
+		fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+			os.Getenv("MYSQL_USER"),
+			os.Getenv("MYSQL_PASS"),
+			os.Getenv("MYSQL_HOST"),
+			os.Getenv("MYSQL_PORT"),
+			os.Getenv("MYSQL_DB"),
+		))
+
 	defer db.Close()
 
 	if err != nil {
@@ -25,4 +41,14 @@ func main() {
 	}
 
 	fmt.Println(version)
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("0.0.0.0:8060")
 }
